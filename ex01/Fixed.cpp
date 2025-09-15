2fdc1dc645c93a856baae4806e10f0f25ed839e7
Arman Arakelyan <armarake@c1r1s3.42yerevan.am>
2025-09-15 16:11:55 +0400

    ex00 changes, ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,6 +6,18 @@ Fixed::Fixed()
 	_value = 0;
 }
 
+Fixed::Fixed(int const num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_value = num * (1 << Fixed::_fractionalBits);
+}
+
+Fixed::Fixed(float const num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_value = (int)roundf(num * (1 << Fixed::_fractionalBits));
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
@@ -36,4 +48,19 @@ void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	_value = raw;
+}
+
+float	Fixed::toFloat() const
+{
+	return (_value / (float)(1 << Fixed::_fractionalBits));
+}
+
+int		Fixed::toInt( void ) const
+{
+	return (_value / (1 << Fixed::_fractionalBits));
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& obj) {
+	out << obj.toFloat();
+	return out;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,6 +1,7 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <cmath>
 # include <iostream>
 
 class Fixed
@@ -10,11 +11,17 @@ private:
 	static int const	_fractionalBits = 8;
 public:
 	Fixed();
+	Fixed (int const num);
+	Fixed (float const num);
 	Fixed(const Fixed &other);
 	~Fixed();
 	Fixed	&operator=(const Fixed &other);
 	int		getRawBits() const ;
 	void	setRawBits(int const raw);
+	float	toFloat() const ;
+	int		toInt() const ;
 };
+
+std::ostream& operator<<(std::ostream& out, const Fixed& obj);
 
 #endif
